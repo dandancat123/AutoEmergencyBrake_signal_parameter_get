60cc188d9c1fc22ce50f9a90318d5e27a10f240b
lxp <lxp@ufee.local>
2018-11-26 09:37:18 +0800

    测试循环时间

--- signal_test/main.c
+++ signal_test/main.c
@@ -7,6 +7,20 @@
 float v, dis_rel, pres, v_d[calc_t], lca_d[calc_t], pres_d [calc_t], acce_d[calc_t];
 
 int main(int argc, const char * argv[]) {
+    clock_t begin, end;                                     //计时-开始
+    double cost;
+    begin = clock();
+    
     printf("Hello, World!\n");
+    
+    int i;
+    for(i=0;i<500;i++){
+        v_d[i] = i;
+    }
+    
+    end = clock();                                          //计时-结束
+    cost = (double)(end - begin)/CLOCKS_PER_SEC*1000;
+    printf("\nconstant CLOCKS_PER_SEC is: %ld, time cost is: %lf msecs\n", CLOCKS_PER_SEC/1000, cost);
+    
     return 0;
 }
